aef4c03ebff983f3441edde35f7c42c503eadb67
Ricky Dunlop <ricky@rehabstudio.com>
2013-04-05 15:57:04 +0100

    Setup config for Heroku

--- app/js/config.js
+++ app/js/config.js
@@ -1,8 +1,8 @@
 define([], function() {
   var config = {};
-  config.apiKey = '';
+  config.apiKey = app.settings.API_KEY;
   config.scopes = 'https://www.googleapis.com/auth/tasks https://www.googleapis.com/auth/userinfo.profile';
-  config.clientId = '';
+  config.clientId = app.settings.CLIENT_ID;
 
   _.templateSettings = {
     interpolate: /\{\{(.+?)\}\}/g
